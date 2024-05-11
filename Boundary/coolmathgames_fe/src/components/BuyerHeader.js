2aab5825995171199a6fc452baeb57a5e2559646
geethoobz <tiffwyn02@gmail.com>
2024-05-11 15:05:01 +0800

    edited buyer header

--- Boundary/coolmathgames_fe/src/components/BuyerHeader.js
+++ Boundary/coolmathgames_fe/src/components/BuyerHeader.js
@@ -51,8 +51,8 @@ function BuyerHeader() {
 
             {/* words on top */}
             <Navbar.Collapse>
-                <Navbar.Link href="/BuyerRetrieveNewListingListUI" className='text-gray-900 hover:text-yellow-700'>Find New Properties</Navbar.Link>
-                <Navbar.Link href="/BuyerRetrieveSoldListingListUI" className='text-gray-900 hover:text-yellow-700'>Find Sold Properties</Navbar.Link>
+                <Navbar.Link href="/BuyerRetrieveNewListingListUI" className='text-gray-900 hover:text-yellow-700'>New Properties</Navbar.Link>
+                <Navbar.Link href="/BuyerRetrieveSoldListingListUI" className='text-gray-900 hover:text-yellow-700'>Sold Properties</Navbar.Link>
                 <Navbar.Link href="/BuyerRetrieveREAListUI" className='text-gray-900 hover:text-yellow-700'>Find Real Estate Agent</Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
