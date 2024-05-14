bcb394eab8420cfc65cfa8a37a62d936b25b909f
jeslynw <wangsajeslyn@gmail.com>
2024-05-14 23:59:44 +0800

    added 300 data favorite, change frontend

--- Boundary/coolmathgames_fe/src/pages/BuyerRetrieveSoldListingListUI.js
+++ Boundary/coolmathgames_fe/src/pages/BuyerRetrieveSoldListingListUI.js
@@ -10,91 +10,7 @@ import axios from 'axios';
 // assets
 // background image
 import BG from "../assets/bg1-30.jpg";
-// property images
-import Prop1 from "../assets/prop1.jpg";
-import Prop2 from "../assets/prop2.jpg";
-import Prop3 from "../assets/prop3.jpg";
-import Prop4 from "../assets/prop4.jpg";
-// agent images
-import Agent1 from "../assets/agent1.jpg";
-import Agent2 from "../assets/agent2.jpg";
-import Agent3 from "../assets/agent3.jpg";
 
-
-// Example: create Agent object
-const agent1 = {
-    id: 1,
-    name: "Peter McProperties",
-    email: "",
-    pfp: Agent1,
-};
-
-const agent2 = {
-    id: 2,
-    name: "Hedge Dea Bee",
-    email: "",
-    pfp: Agent2,
-};
-
-const agent3 = {
-    id: 3,
-    name: "Nuñez",
-    email: "",
-    pfp: Agent3,
-};
-
-// Example: create a property object
-const property1 = {
-    id: 1,
-    images: [Prop1],
-    title: "Trellis Towers Condo Unit",
-    location: "Punggol, Singapore",
-    bedrooms: 3,
-    bathrooms: 2,
-    size: 1000,
-    price: 200000,
-    agent: agent1,
-    isSold: false
-};
-
-const property2 = {
-    id: 2,
-    images: [Prop2],
-    title: "House",
-    location: "Home Street",
-    bedrooms: 3,
-    bathrooms: 2,
-    size: 12500,
-    price: 1850000,
-    agent: agent2,
-    isSold: true
-};
-
-const property3 = {
-    id: 3,
-    images: [Prop3],
-    title: "[1 room] 308 Jurong East Street 4 with many",
-    location: "Jurong East",
-    bedrooms: 1,
-    bathrooms: 10,
-    size: 500,
-    price: 20000,
-    agent: agent3,
-    isSold: false
-};
-
-const property4 = {
-    id: 4,
-    images: [Prop4],
-    title: "4 Room Flat Tampines",
-    location: "Tampines",
-    bedrooms: 3,
-    bathrooms: 90,
-    size: 9032,
-    price: 50,
-    agent: agent2,
-    isSold: true
-};
 
 function BuyerRetrieveSoldListingListUI(props) {
     const [soldProperties, setSoldProperties] = useState([]);
@@ -152,6 +68,9 @@ function BuyerRetrieveSoldListingListUI(props) {
                         <CardProperty property={property}/>
                     ))}
                 </div>
+
+                <div className='p-10'></div>
+
             </div>
 
             <Footer />
