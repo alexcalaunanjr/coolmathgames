462bfc20e317fd0c4098d115d206367cbf306418
jeslynw <wangsajeslyn@gmail.com>
2024-05-15 00:55:49 +0800

    fix render

--- Boundary/coolmathgames_fe/src/pages/BuyerViewNewListingUI.js
+++ Boundary/coolmathgames_fe/src/pages/BuyerViewNewListingUI.js
@@ -62,6 +62,7 @@ function BuyerViewNewListingUI(props) {
             .then((response) => {
                 if (response) {
                     setLoad(false);
+                    console.log("test");
                     setTitle(response.data.propertyName);
                     setLocation(response.data.location);
                     setPrice(response.data.price);
