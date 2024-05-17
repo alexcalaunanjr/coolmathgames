e4afcca24b2fda2815a98ca1b65305ab99fdf42e
chrisyellagracia <83775091+chrisyellagracia@users.noreply.github.com>
2024-05-17 11:41:54 +0800

    modify frontend SellerViewListingUI.js

--- Boundary/coolmathgames_fe/src/pages/SellerViewListingUI.js
+++ Boundary/coolmathgames_fe/src/pages/SellerViewListingUI.js
@@ -101,9 +101,9 @@ function SellerViewPropertyListingUI(props) {
             backgroundSize: "cover",
           }}
         >
-
-          {/* Property Image */}
-          <div className="relative">
+        
+        {/* Property Image */}
+        <div className="relative">
             <div>
               <img
                 src={`data:image/jpeg;base64, ${image}`}
@@ -119,80 +119,78 @@ function SellerViewPropertyListingUI(props) {
             )}
           </div>
 
-          <div className="flex pb-10">
-            <div className="w-1/3 pt-10">
-
-              {/* Property Name */}
-              <div className="flex">
-                <p className="lg:text-3xl md:text-md font-bold">{title}</p>
-              </div>
-
-              <div className="flex pt-1">
-                {/* Location */}
-                <svg
-                  className="w-6 h-6 text-black dark:text-black mr-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                  />
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"
-                  />
-                </svg>
-                <p className="text-md">{location}</p>
-              </div>
-
-              {/* Bedroom, Bathroom, Area */}
-              <div className="flex pt-5">
-                {/* Bedroom */}
-                <div className="lg:w-1/4">
-                  <div className="w-10">
-                    <svg
-                      className="w-10 h-10 text-black dark:text-black" // Set the desired width and height here
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M3 6a1 1 0 0 1 .993.883L4 7v6h6V8a1 1 0 0 1 .883-.993L11 7h8a3 3 0 0 1 2.995 2.824L22 10v8a1 1 0 0 1-1.993.117L20 18v-3H4v3a1 1 0 0 1-1.993.117L2 18V7a1 1 0 0 1 1-1" />
-                      <path d="M7 8a2 2 0 1 1-1.995 2.15L5 10l.005-.15A2 2 0 0 1 7 8"></path>
-                    </svg>
-                    <p className="pt-2 text-md text-center">{bedrooms}</p>
-                  </div>
+          <div className="flex w-full">
+            <div className="w-2/3 pt-10">
+              <div>
+                {/* Property Name */}
+                <div className="flex">
+                  <p className="lg:text-3xl md:text-md font-bold">{title}</p>
                 </div>
 
-                {/* Bathroom */}
-                <div className="lg:w-1/4">
-                  <div className="w-10">
-                    <svg
-                      className="w-10 h-10 text-black dark:text-black"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 -4 24 28"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M3.5 4.135a1.635 1.635 0 0 1 3.153-.607l.144.358a4.09 4.09 0 0 0-1.38 1.774a4.179 4.179 0 0 0-.02 3.107a.75.75 0 0 0 .995.413l5.96-2.566a.75.75 0 0 0 .402-.963a3.973 3.973 0 0 0-2.132-2.213a3.843 3.843 0 0 0-2.466-.192l-.11-.275A3.135 3.135 0 0 0 2 4.135V11h-.25a.75.75 0 0 0 0 1.5H2v.355c0 .375 0 .595.016.84c.142 2.237 1.35 4.302 3.102 5.652a.745.745 0 0 0-.039.068l-1 2a.75.75 0 0 0 1.342.67l.968-1.935a7.358 7.358 0 0 0 2.58.765c.245.025.394.03.648.04h.007c.74.028 1.464.045 2.126.045c.662 0 1.386-.017 2.126-.045h.007c.254-.01.404-.015.648-.04a7.358 7.358 0 0 0 2.58-.765l.968 1.936a.75.75 0 0 0 1.342-.671l-1-2a.796.796 0 0 0-.038-.068c1.751-1.35 2.96-3.416 3.102-5.652c.015-.245.015-.465.015-.84v-.038c0-.06 0-.123-.004-.18a1.752 1.752 0 0 0-.014-.137h.268a.75.75 0 0 0 0-1.5H3.5z"
-                      ></path>
-                    </svg>
-                    <p className="pt-2 text-md text-center">{bathrooms}</p>
-                  </div>
+                <div className="flex pt-1">
+                  {/* Location */}
+                  <svg
+                    className="w-6 h-6 text-black dark:text-black mr-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                    />
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M17.8 13.938h-.011a7 7 0 1 0-11.464.144h-.016l.14.171c.1.127.2.251.3.371L12 21l5.13-6.248c.194-.209.374-.429.54-.659l.13-.155Z"
+                    />
+                  </svg>
+                  <p className="text-md">{location}</p>
                 </div>
+                {/* Bedroom, Bathroom, Area */}
+                <div className="flex pt-5 pb-5">
+                  {/* Bedroom */}
+                  <div className="lg:w-28">
+                    <div className="w-10">
+                      <svg
+                        className="w-10 h-10 text-black dark:text-black" // Set the desired width and height here
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M3 6a1 1 0 0 1 .993.883L4 7v6h6V8a1 1 0 0 1 .883-.993L11 7h8a3 3 0 0 1 2.995 2.824L22 10v8a1 1 0 0 1-1.993.117L20 18v-3H4v3a1 1 0 0 1-1.993.117L2 18V7a1 1 0 0 1 1-1" />
+                        <path d="M7 8a2 2 0 1 1-1.995 2.15L5 10l.005-.15A2 2 0 0 1 7 8"></path>
+                      </svg>
+                      <p className="pt-2 text-md text-center">{bedrooms}</p>
+                    </div>
+                  </div>
 
-                {/* Area */}
-                <div className="lg-1/4">
-                  <div>
+                  {/* Bathroom */}
+                  <div className="lg:w-28">
+                    <div className="w-10">
+                      <svg
+                        className="w-10 h-10 text-black dark:text-black"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 -4 24 28"
+                      >
+                        <path
+                          fill="currentColor"
+                          d="M3.5 4.135a1.635 1.635 0 0 1 3.153-.607l.144.358a4.09 4.09 0 0 0-1.38 1.774a4.179 4.179 0 0 0-.02 3.107a.75.75 0 0 0 .995.413l5.96-2.566a.75.75 0 0 0 .402-.963a3.973 3.973 0 0 0-2.132-2.213a3.843 3.843 0 0 0-2.466-.192l-.11-.275A3.135 3.135 0 0 0 2 4.135V11h-.25a.75.75 0 0 0 0 1.5H2v.355c0 .375 0 .595.016.84c.142 2.237 1.35 4.302 3.102 5.652a.745.745 0 0 0-.039.068l-1 2a.75.75 0 0 0 1.342.67l.968-1.935a7.358 7.358 0 0 0 2.58.765c.245.025.394.03.648.04h.007c.74.028 1.464.045 2.126.045c.662 0 1.386-.017 2.126-.045h.007c.254-.01.404-.015.648-.04a7.358 7.358 0 0 0 2.58-.765l.968 1.936a.75.75 0 0 0 1.342-.671l-1-2a.796.796 0 0 0-.038-.068c1.751-1.35 2.96-3.416 3.102-5.652c.015-.245.015-.465.015-.84v-.038c0-.06 0-.123-.004-.18a1.752 1.752 0 0 0-.014-.137h.268a.75.75 0 0 0 0-1.5H3.5z"
+                        ></path>
+                      </svg>
+                      <p className="pt-2 text-md text-center">{bathrooms}</p>
+                    </div>
+                  </div>
+
+                  {/* Area */}
+                  <div className="lg:w-28">
                     <svg
                       className="w-10 h-10 text-black dark:text-black"
                       aria-hidden="true"
@@ -209,129 +207,121 @@ function SellerViewPropertyListingUI(props) {
                     <p className="pt-2 text-md text-left">{size} sqft</p>
                   </div>
                 </div>
+                {/* A straight line */}
+                <div className="pb-10">
+                  <hr className="border-t-1 border-black" />
+                </div>
+
+                {/* Description */}
+                <div className="w-2/3 pb-10">
+                  <p className="lg:text-2xl md:text-md font-bold pb-5">
+                    About this property
+                  </p>
+                  <p className="text-md">{description}</p>
+                </div>
+
+                {/* A straight line */}
+                <div className="pb-10">
+                  <hr className="border-t-1 border-black" />
+                </div>
+
+                {/* Tabs */}
+                <div className="pb-10">
+                  <p className="lg:text-2xl md:text-md font-bold pb-5">Amenities</p>
+                  <PLTabs facilities={facilities} unitFeatures={unitFeatures} />
+                </div>
               </div>
             </div>
-          
-            {/* Price */}
-            <div className="w-1/3 pt-10"></div>
-          
-            <div className="w-1/3 pt-10">
-              <div className="flex flex-col justify-center ">
-                <div className="flex justify-end mb-2">
-                  <p className="lg:text-3xl md:text-md font-bold text-center">${price}</p>
-                </div>
-
-                <div className="flex justify-end space-x-4 pt-5">
-                  {/* Favorites */}
-                    <button
-                      className="flex items-center justify-center bg-transparent text-black p-3 border border-black rounded-lg hover:bg-white lg:w-1/3 w-1/2"
-                      onClick={handleFavorites}
-                    >
-                    <svg
-                      className="w-[24px] h-[24px] text-red-500 dark:text-white mr-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z" />
-                    </svg>
-                    Favorites
-                  </button>
-
-                  {/* Pop up for favorites */}
-                  {favoritesPopUp && (
-                    <SellerViewFavorites
-                      propertyName={title}
-                      openModal={favoritesPopUp}
-                      onClose={handleReopenPopUp}
-                      token={Ptoken}
-                    />
-                  )}
-
-                  {/* Views */}
-                  <button
-                    className="flex items-center justify-center bg-transparent text-black p-3 border border-black rounded-lg hover:bg-white lg:w-1/3 w-1/2"
-                    onClick={handleViews}
-                  >
-                    <svg
-                      className="w-[24px] h-[24px] text-gray-800 dark:text-white mr-2"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
-                      />
-                      <path
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
-                    Views
-                  </button>
-
-                  {/* Pop up for views */}
-                  {viewsPopUp && (
-                    <SellerViewViewCountUI
-                      propertyName={title}
-                      openModal={viewsPopUp}
-                      onClose={handleReopenPopUp}
-                      token={Ptoken}
-                    />
-                  )}
-                </div>
+            <div className="w-1/3 pt-10 justify-end">
+              {/*Price*/}
+              <div className="flex justify-end">
+                <p className="lg:text-3xl md:text-md font-bold">${price}</p>
               </div>
-        
+              <div className="flex justify-end space-x-4 pt-5">
+                {/* Favorites */}
+                <button
+                  className="flex items-center justify-center bg-transparent text-black p-3 border border-black rounded-lg hover:bg-white lg:w-1/3 w-1/2"
+                  onClick={handleFavorites}
+                >
+                <svg
+                  className="w-[24px] h-[24px] text-red-500 dark:text-white mr-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z" />
+                </svg>
+                Favorites
+                </button>
+
+                {/* Pop up for favorites */}
+                {favoritesPopUp && (
+                  <SellerViewFavorites
+                    propertyName={title}
+                    openModal={favoritesPopUp}
+                    onClose={handleReopenPopUp}
+                    token={Ptoken}
+                  />
+                )}
+
+                {/* Views */}
+                <button
+                  className="flex items-center justify-center bg-transparent text-black p-3 border border-black rounded-lg hover:bg-white lg:w-1/3 w-1/2"
+                  onClick={handleViews}
+                >
+                  <svg
+                    className="w-[24px] h-[24px] text-gray-800 dark:text-white mr-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
+                    />
+                    <path
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                  Views
+                </button>
+
+                {/* Pop up for views */}
+                {viewsPopUp && (
+                  <SellerViewViewCountUI
+                    propertyName={title}
+                    openModal={viewsPopUp}
+                    onClose={handleReopenPopUp}
+                    token={Ptoken}
+                  />
+                )}
+              </div>
               {/* Contact Agent */}
               <Link to={`/SellerViewREACredentialsUI/${agentUN}`}>
                 <div className="flex justify-end pt-6">
-                  <button className="flex items-center justify-center bg-transparent text-black p-3 border border-black rounded-lg hover:bg-white sm:w-1/3">
+                  <button className="flex items-center justify-center bg-transparent text-black p-2 sm:p-3 border border-black rounded-lg hover:bg-white w-full sm:w-auto">
                     <img
                       src={`data:image/jpeg;base64, ${agentImg}`}
                       alt="Agent"
-                      className="w-10 h-10 rounded-full"
+                      className="w-8 h-8 sm:w-10 sm:h-10 rounded-full"
                     />
-                    <p className="text-md items-center px-3">{agentName}</p>
+                    <p className="text-sm sm:text-md px-2 sm:px-3">{agentName}</p>
                   </button>
                 </div>
               </Link>
             </div>
           </div>
-
-          {/* A straight line */}
-          <div className="w-2/3 pb-10">
-            <hr className="border-t-1 border-black" />
-          </div>
-
-          {/* Description */}
-          <div className="w-2/3 pb-10">
-            <p className="lg:text-2xl md:text-md font-bold pb-5">
-              About this property
-            </p>
-            <p className="text-md">{description}</p>
-          </div>
-
-          {/* A straight line */}
-          <div className="w-2/3 pb-10">
-            <hr className="border-t-1 border-black" />
-          </div>
-
-          {/* Tabs */}
-          <div className="pb-10">
-            <p className="lg:text-2xl md:text-md font-bold pb-5">Amenities</p>
-            <PLTabs facilities={facilities} unitFeatures={unitFeatures} />
-          </div>
         </div>
-
+        
         {/* Footer */}
         <Footer />
       </>
